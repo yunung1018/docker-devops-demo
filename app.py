08ed3bcf11b4f92da3ce98271abdd9b9114f1384
yunung1018 <kennyzaq12wsx@gmail.com>
2025-11-04 23:17:52 +0800

    upadate web

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ GREETING = os.environ.get("GREETING", "Hello")
 
 @app.route("/")
 def hello_world():
-    return {"message": f"{GREETING}, Docker!"}
+    return {"message": f"{GREETING}, 這是我的第一次全自動部署！"}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
